253110e20fa20ab8b6df3120a9e7cb88bcf1c33b
Highyard <benj1619@stud.kea.dk>
2019-05-21 14:21:32 +0200

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 
 def main():
 
-  userInput = input("")
-  #Test
+  homeInput = input('1. See Top 20 coins\n' 
+                     '2. My Coins\n')
+  
+  while(True):
 
 
-if __name__ == 'main':
+    if(homeInput == "1"):
+      top20Input = input('1. Search for Coin\n'
+                         '2. Sort by name\n'
+                         '3. Sort by daily change\n'
+                         '4. Go back\n')
+
+      if(top20Input == "1"):
+        saveCoinInput = input()
+      if(top20Input == "2"):
+
+      if(top20Input == "3"):
+
+      if(top20Input == "4"):
+        break
+
+  
+    if(userInput == "q"):
+      break
+
+if __name__ == '__main__':
   main()
